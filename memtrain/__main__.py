cb9c66a1d204a05368d47bad38694b6ef6b14f27
Ian Dorsey <ian.dorsey@gmail.com>
2020-07-04 17:48:26 -0700

    Implemented much of the basic functionality

--- memtrain/__main__.py
+++ memtrain/__main__.py
@@ -3,6 +3,8 @@ import csv
 import random
 import decimal
 import time
+import string
+import textwrap
 from datetime import timedelta
 from statistics import mean
 
@@ -10,6 +12,10 @@ import os
 
 # SettingError ################################################################
 class SettingError(Exception):
+    pass
+
+# NoCueError ##################################################################
+class NoCueError(Exception):
     pass
 
 # Settings ####################################################################
@@ -120,10 +126,26 @@ def train(args):
     settings = MtSettings()
 
     indicies = dict()
-    indicies['cue'] = 0
-    indicies['response'] = 0
+    indicies['cue'] = -1
+    indicies['response'] = -1
+    indicies['mtag'] = []
 
     header_row = 0
+
+    # Set level, if one was specified:
+    if args.level:
+        if args.level == 1:
+            settings.settings['level1'] = True
+            settings.settings['level2'] = False
+            settings.settings['level3'] = False
+        elif args.level == 2:
+            settings.settings['level1'] = False
+            settings.settings['level2'] = True
+            settings.settings['level3'] = False
+        elif args.level == '3':
+            settings.settings['level1'] = False
+            settings.settings['level2'] = False
+            settings.settings['level3'] = True
 
     for row_number, row in enumerate(database):
         # Only one non-empty string in row
@@ -145,18 +167,42 @@ def train(args):
             if 'cue' in this_row and 'response' in this_row:
                 indicies['cue'] = this_row.index('cue')
                 indicies['response'] = this_row.index('response')
+                indicies['tag'] = [index for index, element in enumerate(this_row) if element == 'tag']
+                indicies['mtag'] = [index for index, element in enumerate(this_row) if element == 'mtag']
                 header_row = row_number
             
             break
 
-    # Read cue/response pairs
+    # Read cue/response pairs, tags, and mtags
     for row_number, row in enumerate(database[header_row + 1:]):
         cue = row[indicies['cue']]
         response = row[indicies['response']]
-        cr_database.append([cue, response])
+        tags = []
+        mtags = []
+        for index in indicies['tag']:
+            tags.append(row[index])
+        for index in indicies['mtag']:
+            mtags.append(row[index])
+        cr_database.append({'cue': cue, 'response': response, 'tags': tags,
+        'mtags': mtags})
+
+    if args.tags:
+        args_tags = args.tags.split(',')
+        cr_database = [row for row in cr_database if not set(args_tags).isdisjoint(row['tags'])]
+
+    # Map mtags to responses
+    mtag_list = dict()
+    for row in cr_database:
+        these_mtags = row['mtags']
+        for this_mtag in these_mtags:
+            response = row['response']
+            if this_mtag not in mtag_list.keys():
+                mtag_list[this_mtag] = [response]
+            else:
+                mtag_list[this_mtag].append(response)
 
     # Get responses and aliases
-    responses = [i[1] for i in cr_database]
+    responses = [i['response'] for i in cr_database]
     aliases = dict()
 
     if settings.settings['alias']:
@@ -205,26 +251,102 @@ def train(args):
     question_number = 1
     total = len(cr_database)
 
-    # Prompts
-    for cue, response in cr_database:
+    # Prompts #################################################################
+    for this_dict in cr_database:
+        cue = this_dict['cue']
+        response = this_dict['response']
+        mtags = this_dict['mtags']
+        # Get number of mtags
+        n_mtags = len(mtags)
+
+        # Format the cue
+        f_cue = cue.replace('{{}}', '_________')
+        f_cue = textwrap.fill(f_cue, initial_indent='    ', subsequent_indent='    ', width=80)
+
+        # Print how questions were answered correctly so far if we are not on
+        # the first question.
         if question_number is not 1:
             percentage = decimal.Decimal(correct) / decimal.Decimal(question_number-1) * decimal.Decimal(100)
 
-        print(settings.settings['title'].ljust(29) + iam + 'Cue ' + str(question_number) + '/' + str(total))
+        # Print first two rows and the cue.
+        print(settings.settings['title'].ljust(69) + iam + ('Cue ' + str(question_number) + '/' + str(total)).rjust(10))
         if question_number is not 1:
-            print('Correct so far'.ljust(29) + iam + str(correct) + '/' + str(question_number-1) + ' (' + str(round(percentage, 1)) + '%)')
+            print('Correct so far'.ljust(59) + iam + (str(correct) + '/' + str(question_number-1) + ' (' + str(round(percentage, 1)) + '%)').rjust(20))
         print()
-        print('    ' + cue)
+        print(f_cue)
         print()
+
+        # If on level one, generate the multiple choice questions.
+        if settings.settings['level1']:
+            # First, make copies of sets of responses for each mtag.
+            response_copies_by_mtag = dict()
+
+            for mtag in mtags:
+                # Delete the correct reponse
+                response_copies_by_mtag[mtag] = [i for i in mtag_list[mtag] if i is not response]
+
+            # How many multiple choices will there be?
+            ascii_range = string.ascii_lowercase[:4]
+            
+            # Select the index for the correct choice at random.
+            correct_index = random.choice(ascii_range)
+
+            # Now, for each choice, print the response.
+            for i in ascii_range:
+                # Reset and shuffle copied data.
+                these_response_copies_by_mtag = dict(response_copies_by_mtag)
+                for key in response_copies_by_mtag.keys():
+                    random.shuffle(response_copies_by_mtag[key])
+                these_mtags = list(mtags)
+                random.shuffle(these_mtags)
+                # If i is the correct_index, print the correct response
+                if i == correct_index:
+                    # Print the capitalized correct response.
+                    print((i + ')').ljust(5) + iam + response.capitalize())
+                # Otherwise, select an incorrect response at random.
+                else:
+                    this_mtag = random.choice(these_mtags)
+                    # If there are no more mtags for this_mtag, delete it from
+                    # these_mtags.
+                    while len(these_response_copies_by_mtag[this_mtag]) == 0:
+                        these_mtags.remove(this_mtag)
+                        # If these_mtags is empty, just choose a random mtag.
+                        if len(these_mtags) == 0:
+                            this_mtag = random.choice(list(mtag_list.keys()))
+                            these_response_copies_by_mtag[this_mtag] = [i for i in mtag_list[this_mtag] if i is not response]
+                    # Get a random response.
+                    this_response = these_response_copies_by_mtag[this_mtag].pop()
+                    # If the response is the same as the correct response,
+                    # get another one.
+                    while this_response == response:
+                        this_response = these_response_copies_by_mtag[this_mtag].pop()
+                    # Print the capitalized response
+                    print((i + ')').ljust(5) + iam + this_response.capitalize())
+
+            print()
+
+        # Start time
         start = time.time()
-        answer = input('Enter response: ')
+        # Prompt for a response
+        user_input = input('Enter response: ')
+        # End time
         end = time.time()
+        # Get elapsed time and add it to times.
         elasped_time = end - start
         times.append(elasped_time)
         print()
+        # Clear screen.
         os.system('clear')
 
-        if response == answer or response == aliases[answer]:
+        if settings.settings['level1']:
+            correct_user_input = correct_index == user_input
+        elif settings.settings['level3']:
+            if user_input in aliases.keys():
+                correct_user_input = response.lower() == user_input.lower() or response == aliases[user_input.lower()]
+            else:
+                correct_user_input = response.lower() == user_input.lower()
+
+        if correct_user_input:
             print('Correct.')
             correct = correct + 1
         else:
@@ -254,6 +376,8 @@ parser = argparse.ArgumentParser(
 parser.set_defaults(func=train)
 
 # Create arguments
+parser.add_argument('-t', '--tags', help='Study these tags only')
+parser.add_argument('-l', '--level', help='Specify which level to study')
 parser.add_argument('csvfile', help='The CSV file to load')
 
 # Parse arguments
