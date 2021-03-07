d947f19f4be1c9544038f125a22fc7bc88d81605
Ian Dorsey <ian.dorsey@gmail.com>
2021-03-07 10:31:58 -0800

    GUI: More comments

--- memtrain/memtrain_gui/__main__.py
+++ memtrain/memtrain_gui/__main__.py
@@ -14,7 +14,7 @@ from memtrain.memtrain_common.question import Question
 
 class MemtrainGUI:
     def __init__(self):
-        '''Construct the settings interface'''
+        '''Initialize main variables and the configuration window'''
         self.root = tk.Tk()
         self.root.minsize(300, 100)
         self.root.title('memtrain v0.3a')
@@ -27,6 +27,10 @@ class MemtrainGUI:
 
         self.start_time = None
         self.end_time = None
+
+        #
+        # Configuration window
+        #
 
         # Settings ############################################################
         self.settings_frame = tk.Frame(master=self.root)
@@ -86,6 +90,7 @@ class MemtrainGUI:
         start_level_2 = partial(self.start_level, '2')
         start_level_3 = partial(self.start_level, '3')
 
+        # Wait until CSV is loaded to change buttons into normal state.
         self.step_2_button_frame = tk.Frame(master=self.step_2_frame)
         self.level_1_button = tk.Button(master=self.step_2_button_frame, text='Level 1',
                                         padx=10, state=tk.DISABLED, command=start_level_1)
@@ -101,6 +106,7 @@ class MemtrainGUI:
         self.step_2_frame.pack(fill=tk.X, expand=tk.YES, pady=(0, 10))
 
     def configure_settings(self):
+        '''A window for more advanced settings'''
         self.settings_window = tk.Toplevel(self.root)
         self.settings_window.title('Settings')
 
@@ -149,6 +155,7 @@ class MemtrainGUI:
         if self.not_tags != '':
             self.not_tags_entry.insert(0, self.not_tags)
 
+        # Set focus for better usability.
         self.nquestions_entry.focus_set()
 
     def clear_settings(self):
@@ -165,6 +172,7 @@ class MemtrainGUI:
         self.settings_window.destroy()
 
     def nquestions_validate(self, input):
+        '''Determine what values can be input into the nquestions Entry'''
         if input == '':
             return True
         else:
@@ -177,14 +185,12 @@ class MemtrainGUI:
             except ValueError:
                 return False
 
-    def select_csv(self):
-        self.filename = tk_filedialog.askopenfilename(title='Select a memtrain CSV file',
-                filetypes=(('CSV files', '*.csv'),))
-        self.step_1_filename_label.configure(text='Selected file: ' + self.filename)
-
-        # Initialize Engine
+    def initialize_engine_and_core_objects(self):
+        '''Initialize the engine and core objects'''
+        # Initialize Engine - memtrain.memtrain_common.engine
         self.engine = Engine(self.filename, self.level, self.nquestions, self.tags, self.not_tags)
 
+        # Initialize core objects
         self.settings = self.engine.settings
         self.database = self.engine.database
         self.mtstatistics = self.engine.mtstatistics
@@ -192,10 +198,20 @@ class MemtrainGUI:
 
         self.question = Question(self.settings, self.database)
 
+    def select_csv(self):
+        '''The interface for CSV selection'''
+        self.filename = tk_filedialog.askopenfilename(title='Select a memtrain CSV file',
+                filetypes=(('CSV files', '*.csv'),))
+        self.step_1_filename_label.configure(text='Selected file: ' + self.filename)
+
+        self.initialize_engine_and_core_objects()
+
+        # Whether or not levels are enabled
         settings_truth_list = [self.settings.settings['level1'],
                                self.settings.settings['level2'],
                                self.settings.settings['level3']]
 
+        # Number of levels enabled
         only_one_enabled_level = sum(settings_truth_list) == 1
 
         # Enable buttons. If there is only enabled level, just start it
@@ -229,15 +245,7 @@ class MemtrainGUI:
         '''Start a level'''
         self.level = level
         
-        # Initialize Engine
-        self.engine = Engine(self.filename, self.level, self.nquestions, self.tags, self.not_tags)
-
-        self.settings = self.engine.settings
-        self.database = self.engine.database
-        self.mtstatistics = self.engine.mtstatistics
-        self.cr_id_pairs = self.engine.cr_id_pairs
-
-        self.question = Question(self.settings, self.database)
+        self.initialize_engine_and_core_objects()
 
         # Create the training window
         self.training_window = tk.Toplevel(self.root)
@@ -338,7 +346,7 @@ class MemtrainGUI:
         '''Triggered by <Key>, before sending the event to the widget'''
         # If the key pressed isn't a special key, remove placeholder and
         # unitalicize widget text
-        if(self.response_entry_placeholder == True and event.char != ""):
+        if self.response_entry_placeholder == True and event.char != "":
             self.response_entry.delete(0, tk.END)
             self.response_entry.configure(font='Arial 9')
             self.response_entry_placeholder = False
@@ -346,7 +354,7 @@ class MemtrainGUI:
     def response_keyrelease(self, event=None):
         '''Triggered by <KeyRelease>, before sending the event to the widget'''
         # If widget is empty, italicize font, then insert placeholder
-        if(self.response_entry_placeholder == False and len(self.response_entry.get()) == 0):
+        if self.response_entry_placeholder == False and len(self.response_entry.get()) == 0:
             self.response_entry.configure(font='Arial 9 italic')
             self.response_entry.insert(0, 'Enter response')
             self.response_entry_placeholder = True
@@ -354,10 +362,11 @@ class MemtrainGUI:
 
     def response_focusin(self, event=None):
         '''Triggered by placing focus on the widget'''
-        if(self.response_entry_placeholder == True):
+        if self.response_entry_placeholder == True:
             self.response_entry.icursor(0)
 
     def response_clear(self):
+        '''Clear the response Entry'''
         self.response_entry.delete(0, tk.END)
         self.response_entry.configure(font='Arial 9 italic')
         self.response_entry.insert(0, 'Enter response')
@@ -365,14 +374,22 @@ class MemtrainGUI:
         self.response_entry.icursor(0)
 
     def set_cue_text_widget_content(self, content):
+        '''Set cue Text widget content'''
         self.cue_text_widget.delete(1.0, tk.END)
         self.cue_text_widget.insert(tk.END, content)
 
     def render_question(self):
+        '''Render the question and process the necessary data to do so'''
         # Data processing #####################################################
+        # Important: The question we are on
         this_question_id = self.mtstatistics.response_number-1
+
+        # See memtrain.memtrain_common.question
+        # This method is responsible for processing the data needed to render
+        # the question.
         self.question.main_data_loop(self.cr_id_pairs[this_question_id][0], self.cr_id_pairs[this_question_id][1], self.mtstatistics)
 
+        # Change Labels and Text widget text
         self.title_label.configure(text=self.question.title_text)
         self.level_label.configure(text=self.question.level_text)
         self.response_number_label.configure(text=self.question.response_number_text)
@@ -390,11 +407,15 @@ class MemtrainGUI:
 
             counter = 0
 
+            # Store mchoice data in dictionaries
             self.mchoice_buttons = dict()
             mchoice_commands = dict()
             self.mchoice_labels = dict()
+
+            # Iterate through mchoices
             for letter, choice in self.question.mchoices.items():
                 mchoice_commands[letter] = partial(self.submit, mchoice_letter=letter)
+                # Make it so you can press a, b, c, d to select a choice
                 self.training_window.bind(letter, mchoice_commands[letter])
                 self.mchoice_buttons[letter] = tk.Button(master=self.mchoices_frame, text=letter,
                                                     padx=10, command=mchoice_commands[letter])
@@ -410,18 +431,22 @@ class MemtrainGUI:
         self.start_time = time.time()
 
     def submit(self, event=None, mchoice_letter=None):
+        '''Submit response'''
         if self.level == '1':
             self.question.user_input = mchoice_letter
         else:
             self.question.user_input = self.response_entry.get().lower()
 
+        # Validate input
         self.question.validate_input()
 
         if self.mtstatistics.is_input_valid:
+            # Calculate the amount of time elapsed and store it
             self.end_time = time.time()
             elapsed_time = self.end_time - self.start_time
             self.mtstatistics.times.append(elapsed_time)
 
+            # Grade input and do finalization work
             self.question.grade_input()
             self.question.finalize()
             self.correctness_label.configure(text=self.question.correctness_str)
@@ -434,7 +459,9 @@ class MemtrainGUI:
         else:
             tk_messagebox.showinfo('Invalid response', 'Sorry, your response is not valid. Please try again.')
 
+        # Clean up after a question
         if not self.mtstatistics.is_last_question():
+            # Clear response areas
             if self.level == '1':
                 for letter, mchoice_button in self.mchoice_buttons.items():
                     mchoice_button.destroy()
@@ -443,7 +470,10 @@ class MemtrainGUI:
             else:
                 self.response_clear()
 
+            # Get ready for the next question
             self.render_question()
+        # If that was the last question, destroy the training window and
+        # display statistics.
         else:
             self.training_window.destroy()
 
@@ -457,6 +487,8 @@ class MemtrainGUI:
             
             tk_messagebox.showinfo('Training session complete', msg_box_str)
             
+            # Set the level button focus in the configuration window to the
+            # button for the level just completed
             if self.settings.level == '1':
                     self.level_1_button.focus_set()
             elif self.settings.level == '2':
